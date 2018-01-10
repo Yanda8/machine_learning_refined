4c61430141900f92ec132a21fa37f58b6696df55
Jeremy Watt <jermwatt@gmail.com>
2018-01-10 12:43:37 -0600

    multi updates

--- mlrefined_libraries/math_optimization_library/optimizers.py
+++ mlrefined_libraries/math_optimization_library/optimizers.py
@@ -1,6 +1,7 @@
 import autograd.numpy as np
 from autograd import value_and_grad 
 from autograd import hessian
+from autograd.misc.flatten import flatten_func
 
 # random search function
 def random_search(g,alpha_choice,max_its,w,num_samples):
@@ -176,13 +177,16 @@ def gradient_descent(g,alpha_choice,max_its,w):
 
 # newtons method function - inputs: g (input function), max_its (maximum number of iterations), w (initialization)
 def newtons_method(g,max_its,w,**kwargs):
+    # flatten input funciton, in case it takes in matrices of weights
+    flat_g, unflatten, w = flatten_func(g, w)
+    
     # compute the gradient / hessian functions of our input function -
     # note these are themselves functions.  In particular the gradient - 
     # - when evaluated - returns both the gradient and function evaluations (remember
     # as discussed in Chapter 3 we always ge the function evaluation 'for free' when we use
     # an Automatic Differntiator to evaluate the gradient)
-    gradient = value_and_grad(g)
-    hess = hessian(g)
+    gradient = value_and_grad(flat_g)
+    hess = hessian(flat_g)
     
     # set numericxal stability parameter / regularization parameter
     epsilon = 10**(-7)
@@ -195,7 +199,7 @@ def newtons_method(g,max_its,w,**kwargs):
     for k in range(max_its):
         # evaluate the gradient, store current weights and cost function value
         cost_eval,grad_eval = gradient(w)
-        weight_history.append(w)
+        weight_history.append(unflatten(w))
         cost_history.append(cost_eval)
 
         # evaluate the hessian
@@ -208,9 +212,9 @@ def newtons_method(g,max_its,w,**kwargs):
         w = w - np.dot(np.linalg.pinv(hess_eval + epsilon*np.eye(np.size(w))),grad_eval)
 
     # collect final weights
-    weight_history.append(w)
+    weight_history.append(unflatten(w))
     # compute final cost function value via g itself (since we aren't computing 
     # the gradient at the final step we don't get the final cost function value 
     # via the Automatic Differentiatoor) 
-    cost_history.append(g(w))  
+    cost_history.append(flat_g(w))  
     return weight_history,cost_history
