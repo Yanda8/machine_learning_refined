25c49716878c7ab0e800197048404db037129d59
Jeremy Watt <jermwatt@gmail.com>
2018-01-16 09:34:48 -0600

    test

--- mlrefined_libraries/superlearn_library/normalizers.py
+++ mlrefined_libraries/superlearn_library/normalizers.py
@@ -6,11 +6,8 @@ def standard_normalizer(x):
     x_means = np.mean(x,axis = 1)[:,np.newaxis]
     x_stds = np.std(x,axis = 1)[:,np.newaxis]   
 
-    # create normalizer and input normalizer functions based on mean / std
+    # create standard normalizer based on mean / std
     normalizer = lambda data: (data - x_means)/x_stds
 
-    # create inverse normalizer function 
-    inverse_normalizer = lambda data: data*x_stds + x_means
-
     # return normalizer and inverse_normalizer
-    return normalizer,inverse_normalizer
+    return normalizer
--- mlrefined_libraries/unsupervised_library/normalizers.py
+++ mlrefined_libraries/unsupervised_library/normalizers.py
@@ -10,17 +10,11 @@ class My_Normalizers:
     For each scheme you put in input features, and the following is returned
     - normalizer: the normalization scheme of your choice, returned as a function that 
     you can then use for future test points
-    - inverse_normalizer: inverse normalization function for reversing the chosen 
-    normalization
     
     You can then normalize the input x of a dataset using the desired normalization scheme
     by 
     
     x_normalized = normalizer(x)
-    
-    and then return the data to its original form as
-    
-    x_orig = inverse_normalizer(x_normalized)
     '''    
 
     ###### standard normalization function ######
@@ -29,15 +23,11 @@ class My_Normalizers:
         x_means = np.mean(x,axis = 1)[:,np.newaxis]
         x_stds = np.std(x,axis = 1)[:,np.newaxis]   
 
-        # create normalizer and input normalizer functions based on mean / std
+        # create standard normalizer function
         normalizer = lambda data: (data - x_means)/x_stds
 
-        # create inverse normalizer function 
-        inverse_normalizer = lambda data: data*x_stds + x_means
-
-        # return normalizer and inverse_normalizer
-        return normalizer,inverse_normalizer
-
+        # return normalizer 
+        return normalizer
 
     ###### standard normalization function ######
     # compute eigendecomposition of data covariance matrix
@@ -58,7 +48,7 @@ class My_Normalizers:
     # PCA-sphereing - use PCA to normalize input features
     def PCA_sphereing(self,x,**kwargs):
         # standard normalize the input data
-        standard_normalizer, inv_standard_normalizer = self.standard(x)
+        standard_normalizer = self.standard(x)
         x_standard = standard_normalizer(x)
         
         # compute pca transform 
@@ -68,13 +58,7 @@ class My_Normalizers:
         D_ = np.array([1/d**(0.5) for d in D])
         D_ = np.diag(D_)
         W = np.dot(D_,V.T)
-        normalizer = lambda data: np.dot(W,standard_normalizer(data))
+        pca_sphere_normalizer = lambda data: np.dot(W,standard_normalizer(data))
 
-        # create inverse sphereical transform
-        Dinv = np.array([d**(0.5) for d in D])
-        Dinv = np.diag(Dinv)
-        Winv = np.dot(V,Dinv)
-        inverse_normalizer = lambda data: inv_standard_normalizer(np.dot(Winv,data))
-
-        # return normalizer / inverse normalizer
-        return normalizer, inverse_normalizer
+        # return normalizer 
+        return pca_sphere_normalizer
