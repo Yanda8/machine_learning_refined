416bf0ef99058c926ff8d621057cad35f8871f0a
Jeremy Watt <jermwatt@gmail.com>
2018-02-28 17:25:53 -0600

    boost update

--- mlrefined_libraries/nonlinear_superlearn_library/early_stop_lib/superlearn_setup.py
+++ mlrefined_libraries/nonlinear_superlearn_library/early_stop_lib/superlearn_setup.py
@@ -18,8 +18,8 @@ class Setup:
         self.weight_histories = []
         self.train_cost_histories = []
         self.train_count_histories = []
-        self.test_cost_histories = []
-        self.test_count_histories = []
+        self.valid_cost_histories = []
+        self.valid_count_histories = []
         
     #### define feature transformation ####
     def choose_features(self,name,**kwargs): 
@@ -67,20 +67,20 @@ class Setup:
         self.x = self.normalizer(self.x)
         self.normalizer_name = name
        
-    #### split data into training and testing sets ####
-    def make_train_test_split(self,train_portion):
+    #### split data into training and validation sets ####
+    def make_train_valid_split(self,train_portion):
         # translate desired training portion into exact indecies
         r = np.random.permutation(self.x.shape[1])
         train_num = int(np.round(train_portion*len(r)))
         self.train_inds = r[:train_num]
-        self.test_inds = r[train_num:]
+        self.valid_inds = r[train_num:]
         
-        # define training and testing sets
+        # define training and validation sets
         self.x_train = self.x[:,self.train_inds]
-        self.x_test = self.x[:,self.test_inds]
+        self.x_valid = self.x[:,self.valid_inds]
         
         self.y_train = self.y[:,self.train_inds]
-        self.y_test = self.y[:,self.test_inds]
+        self.y_valid = self.y[:,self.valid_inds]
      
     #### define cost function ####
     def choose_cost(self,name,**kwargs):
@@ -89,28 +89,29 @@ class Setup:
         self.full_cost = funcs.cost
         self.full_model = funcs.model
 
-        # create training and testing cost functions
+        # create training and validation cost functions
         funcs = cost_functions.Setup(name,self.x_train,self.y_train,self.feature_transforms,**kwargs)
         self.cost = funcs.cost
         self.model = funcs.model
         
-        funcs = cost_functions.Setup(name,self.x_test,self.y_test,self.feature_transforms,**kwargs)
-        self.test_cost = funcs.cost
-        
+        funcs = cost_functions.Setup(name,self.x_valid,self.y_valid,self.feature_transforms,**kwargs)
+        self.valid_cost = funcs.cost
+        self.valid_model = funcs.model
+
         # if the cost function is a two-class classifier, build a counter too
         if name == 'softmax' or name == 'perceptron':
             funcs = cost_functions.Setup('twoclass_counter',self.x_train,self.y_train,self.feature_transforms,**kwargs)
             self.counter = funcs.cost
             
-            funcs = cost_functions.Setup('twoclass_counter',self.x_test,self.y_test,self.feature_transforms,**kwargs)
-            self.test_counter = funcs.cost
+            funcs = cost_functions.Setup('twoclass_counter',self.x_valid,self.y_valid,self.feature_transforms,**kwargs)
+            self.valid_counter = funcs.cost
             
         if name == 'multiclass_softmax' or name == 'multiclass_perceptron':
             funcs = cost_functions.Setup('multiclass_counter',self.x_train,self.y_train,self.feature_transforms,**kwargs)
             self.counter = funcs.cost
             
-            funcs = cost_functions.Setup('multiclass_counter',self.x_test,self.y_test,self.feature_transforms,**kwargs)
-            self.test_counter = funcs.cost
+            funcs = cost_functions.Setup('multiclass_counter',self.x_valid,self.y_valid,self.feature_transforms,**kwargs)
+            self.valid_counter = funcs.cost
             
         self.cost_name = name
             
@@ -151,30 +152,30 @@ class Setup:
         if optimizer == 'newtons_method':
             weight_history = optimizers.newtons_method(self.cost,self.max_its,self.w_init,self.num_pts,self.batch_size,epsilon = epsilon)
         
-        # compute training and testing cost histories
+        # compute training and validation cost histories
         train_cost_history = [self.cost(v,np.arange(np.size(self.y_train))) for v in weight_history]
-        test_cost_history = [self.test_cost(v,np.arange(np.size(self.y_test))) for v in weight_history]
+        valid_cost_history = [self.valid_cost(v,np.arange(np.size(self.y_valid))) for v in weight_history]
         
         # store all new histories
         self.weight_histories.append(weight_history)
         self.train_cost_histories.append(train_cost_history)
-        self.test_cost_histories.append(test_cost_history)
+        self.valid_cost_histories.append(valid_cost_history)
         
         # if classification produce count history
         if self.cost_name == 'softmax' or self.cost_name == 'perceptron' or self.cost_name == 'multiclass_softmax' or self.cost_name == 'multiclass_perceptron':
             train_count_history = [self.counter(v) for v in weight_history]
-            test_count_history = [self.test_counter(v) for v in weight_history]
+            valid_count_history = [self.valid_counter(v) for v in weight_history]
 
             # store count history
             self.train_count_histories.append(train_count_history)
-            self.test_count_histories.append(test_count_history)
+            self.valid_count_histories.append(valid_count_history)
  
     #### plot histories ###
     def show_histories(self,**kwargs):
         start = 0
         if 'start' in kwargs:
             start = kwargs['start']
-        history_plotters.Setup(self.train_cost_histories,self.train_count_histories,self.test_cost_histories,self.test_count_histories,start)
+        history_plotters.Setup(self.train_cost_histories,self.train_count_histories,self.valid_cost_histories,self.valid_count_histories,start)
         
     #### for batch normalized multilayer architecture only - set normalizers to desired settings ####
     def fix_normalizers(self,w):
@@ -183,9 +184,9 @@ class Setup:
         self.feature_transforms(self.x,w);
         
         # re-assign feature transformation based on these settings
-        self.testing_feature_transforms = self.transformer.testing_feature_transforms
+        self.validation_feature_transforms = self.transformer.validation_feature_transforms
         
         ### re-assign cost function (and counter) based on fixed architecture ###
-        funcs = cost_functions.Setup(self.cost_name,self.x,self.y,self.testing_feature_transforms)
+        funcs = cost_functions.Setup(self.cost_name,self.x,self.y,self.validation_feature_transforms)
         self.model = funcs.model
     
