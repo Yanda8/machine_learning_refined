c09d4415430f85a26e5347b94a6a196c8b618146
Jeremy Watt <jermwatt@gmail.com>
2018-03-21 02:18:35 -0500

    test

--- mlrefined_libraries/recurrent_library/time_series_animators.py
+++ mlrefined_libraries/recurrent_library/time_series_animators.py
@@ -200,4 +200,63 @@ class Visualizer:
         anim = animation.FuncAnimation(fig, animate ,frames=num_frames, interval=num_frames, blit=True)
         
         return(anim)
-  
+
+    #### animate range of exponential average calculations ####
+    def animate_exponential_average_range(self,x,func,params,**kwargs):
+        # produce figure
+        fig = plt.figure(figsize = (9,4))
+        gs = gridspec.GridSpec(1, 3, width_ratios=[1,7,1]) 
+        ax = plt.subplot(gs[0]); ax.axis('off')
+        ax1 = plt.subplot(gs[1]); 
+        ax2 = plt.subplot(gs[2]); ax2.axis('off')
+        artist = fig
+        
+        # view limits
+        xmin = -3
+        xmax = len(x) + 3
+        ymin = np.min(x)
+        ymax = np.max(x) 
+        ygap = (ymax - ymin)*0.15
+        ymin -= ygap
+        ymax += ygap
+            
+        # start animation
+        num_frames = len(params)+1
+        print ('starting animation rendering...')
+        def animate(k):
+            # clear panels
+            ax1.cla()
+            
+            # print rendering update
+            if np.mod(k+1,25) == 0:
+                print ('rendering animation frame ' + str(k+1) + ' of ' + str(num_frames))
+            if k == num_frames - 1:
+                print ('animation rendering complete!')
+                time.sleep(1.5)
+                clear_output()
+                
+            # plot x
+            ax1.plot(np.arange(1,x.size + 1),x,alpha = 1,c = 'k',linewidth = 1,zorder = 2);
+
+            # create y
+            if k == 0:
+                alpha = params[0]
+                y = func(x,alpha)
+                #ax1.plot(np.arange(1,y.size + 1),y,alpha = 1,c = 'fuchsia',linewidth = 3,zorder = 3);
+                ax1.set_title(r'Original data')
+
+            if k > 0:
+                alpha = params[k-1]
+                y = func(x,alpha)
+                ax1.plot(np.arange(1,y.size + 1),y,alpha = 0.9,c = 'fuchsia',linewidth = 3,zorder = 3);
+                ax1.set_title(r'$\alpha = $ ' + str(np.round(alpha,2)))
+
+            # label axes
+            ax1.set_xlabel(r'$t$',fontsize = 13)
+            ax1.set_xlim([xmin,xmax])
+            ax1.set_ylim([ymin,ymax])
+            return artist,
+
+        anim = animation.FuncAnimation(fig, animate ,frames=num_frames, interval=num_frames, blit=True)
+        
+        return(anim)
