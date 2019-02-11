58e39d00666fec88e6aaebd2d0335f6e75d1518b
Reza Borhani <reza@Rezas-MacBook-Pro.local>
2019-02-11 10:51:09 -0600

    update

--- html/prettify.py
+++ html/prettify.py
@@ -17,64 +17,23 @@ name = soup.html.head.title.string
 
 # change title to page_title
 soup.html.head.title.string = page_title
-
-
-
-# assign series URL
-if 'differentiation' in series.lower():
-    series_url = '3_Automatic_differentiation'
-    
-if 'zero' in series.lower():
-    series_url = '5_Zero_order_methods'
+ 
+   
+series_dict = {
+	"Chapter 2:": "2_Zero_order_methods",
+	"Chapter 3:": "3_First_order_methods",
+	"Chapter 4:": "4_Second_order_methods",
+	"Chapter 5:": "5_Linear_regression",
+	"Chapter 6:": "6_Linear_twoclass_classification",
+	"Chapter 7:": "7_Linear_multiclass_classification",
+	"Chapter 8:": "8_Linear_unsupervised_learning",
+	"Chapter 9:": "9_Feature_engineer_select",
+	"Chapter 10": "10_Nonlinear_intro",
+	"Chapter 13": "13_Multilayer_perceptrons"
+	}
+	
+series_url = series_dict[series[0:10]]	
         
-if 'first' in series.lower():
-    series_url = '6_First_order_methods'    
-    
-if 'second' in series.lower():
-    series_url = '7_Second_order_methods'    
-    
-if 'linear' in series.lower() and 'regression' in series.lower():
-    series_url = '8_Linear_regression'   
-    
-if 'linear' in series.lower() and 'unsupervised' in series.lower():
-    series_url = '11_Linear_unsupervised_learning'
-    
-if 'reinforcement' in series.lower() and 'foundation' in series.lower():
-    series_url = '18_Reinforcement_Learning_Foundations'                           
-  
-if 'multiclass' in series.lower() and 'linear' in series.lower():
-    series_url = '10_Linear_multiclass_classification'    
-
-if 'classification' in series.lower() and 'linear' in series.lower():
-    series_url = '9_Linear_twoclass_classification'  
-
-if 'nonlinear' in series.lower() and 'introduction' in series.lower():
-    series_url = '12_Nonlinear_intro'   
-    
-if 'layer' in series.lower() and 'perceptron' in series.lower():
-    series_url = '13_Multilayer_perceptrons'
-    
-if 'convolution' in series.lower() and 'network' in series.lower():
-    series_url = '14_Convolutional_networks'   
-
-if 'recurrent' in series.lower() and 'network' in series.lower():
-    series_url = '15_Recurrent_Networks'  
-    
-if 'selection' in series.lower() and 'engin' in series.lower():
-    series_url = '9_Feature_engineer_select' 
-    
-if 'nonlinear' in series.lower() and 'feature' in series.lower():
-    series_url = '10_Nonlinear_intro'  
-    
-if 'principle' in series.lower() and 'learning' in series.lower():
-    series_url = '11_Feature_learning'  
-    
-if 'element' in series.lower() and 'algebra' in series.lower():
-    series_url = '16_Computational_linear_algebra'           
-    
-   
-   
-series_url = "2_Zero_order_methods"          
          
 
 # This script adds navigation bar + sharing logos + title
